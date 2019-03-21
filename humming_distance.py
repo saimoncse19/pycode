547109e95e2c875d8a3ab2a31e97e02d77e5424d
Saimon <48386830+saimoncse19@users.noreply.github.com>
2019-03-21 22:04:53 +0600

    Create humming_distance.py
     The Hamming distance between two strings of equal length is the number of positions at which the corresponding symbols are different.

---
+++ humming_distance.py
@@ -0,0 +1,22 @@
+"""" Calculation of Humming Distance
+Author: Saimon
+email: saimoncse19@gmail.com
+Date: March 21, 2019 """
+
+
+def humming_distance(a: str, b: str):
+
+    distance = 0
+
+    if len(a) == len(b):
+        for i, j in zip(a, b):
+            if i != j:
+                distance += 1
+        return distance
+
+    else:
+        return "The two strings are not of equal size..."
+
+
+x = humming_distance("10011010", "10101110")
+print(x)
